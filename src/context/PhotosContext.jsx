74e43c2d0a91b8d9117608fb40b02295b6a5085e
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-02-25 17:26:56 -0300

    hola5

--- src/context/PhotosContext.jsx
+++ src/context/PhotosContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const PhotosContext = createContext();
 
-const PHOTO_URL = "/photos.json";
+const PHOTO_URL = 'photos.json';
 
 const PhotosProvider = ({ children }) => {
 
@@ -12,9 +12,7 @@ const PhotosProvider = ({ children }) => {
   const getPhotos = async () => {
     const response = await fetch(PHOTO_URL);
 
-
     const { photos: photosDB } = await response.json();
-
 
     setPhotos(photosDB.map((photo) => ({ ...photo, isFavorite: false })));
   };
